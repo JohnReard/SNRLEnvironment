Static objects implemented, no collision yet and buggy drawing in animation, but showing as rotated. Will have to fix rotation seeming to be fixed at a certain value, illegal states being drawn and issues with sizes of statobjs. Animation also turns blank for certain seeds.

SNRLEnvironment/Env1.py
@@ -41,7 +41,11 @@ def sfm(objstate, objstates, counter, key):
     objtrans = objmove - jnp.sum(distances)
     #see whether agent stops to "talk" to other agent, or changes goal.
 
-    return key1
+    return key1, objtrans
+@jax.jit
+def checkillegalstate(state):#checks if point is in static object collision
+    
+    pass
 @jax.jit
 def detectdistances(objstate, otherobjs):
     dists = jax.vmap(lambda obj, othobj : othobj - obj, in_axes=(None,0))(objstate, otherobjs)

SNRLEnvironment/batching.py
@@ -15,28 +15,70 @@ def create_env(randint, objrad):
     #unfortunately can't parallelise adding elements to array (or even pytrees)
     return states #returned states is objnum + 2 * [x,y]
 @jax.jit
-def create_statobj(objsizes, rotations, key):
-    jax.random.uniform(key,minval=limits[0]+,maxval=40)
-    objsizes
+def create_centres(objsize, key, limits,statnum):
+    centres = jax.random.uniform(key,shape=(statnum,2),minval=limits[0]+objsize,maxval=limits[1]+objsize)
+    return centres
+    
+@jax.jit
+def return_vertices(centre,objsize,rotation):
+    jax.debug.print("{x} is objsize",x=objsize)
+    theta = rotation * (jnp.pi/180)
+    initvert = ((centre[0] - objsize[0], centre[1] - objsize[1]),
+                (centre[0] + objsize[0], centre[1] - objsize[1]),
+                (centre[0] - objsize[0], centre[1] + objsize[1]),
+                (centre[0] + objsize[0], centre[1] + objsize[1]))
+    #rotate shape, theta is angle in RADIANS
+    newvertices = []
+    dists = []
+    for vertex in initvert:
+        newx = (((centre[0] - vertex[0]) * jnp.cos(theta)) - ((centre[1] - vertex[1]) * jnp.sin(theta)))+ centre[0]
+        newy = (((centre[1] - vertex[1]) * jnp.sin(theta)) + ((centre[0] - vertex[0]) * jnp.cos(theta)) + centre[1])
+        a = centre[0]-vertex[0]
+        b = centre[1]-vertex[1]
+        dists.append([((centre[0] - vertex[0]) * jnp.cos(theta)),((centre[1] - vertex[1]) * jnp.sin(theta))])
+        #distance should be signed?? if it is it can transform vertex to rotate
+        newvertices.append([newx,newy])
+    vertices=jnp.array(newvertices)
+    print("my shape ", jnp.array(dists).shape)
+    #the y is the same for top left and top right, top left should decrease and top right should increase?
+    jax.debug.print("{x} is init, {y} is rotated", x= initvert, y=newvertices)
+    return vertices, jnp.array(dists)
 
 @jax.jit
-def return_states(randints,objrad, objsizes, rotations,centres, limits):
+def create_vertices(rotations, centres, objsizes):
+    #vertices are statobjnum in shape
+    print(centres.shape,objsizes.shape,rotations.shape)
+    vertices=[]
+    vertices, dists = jax.vmap(return_vertices)(centres,objsizes,rotations)#statobjnum shape
+    return vertices, dists
+@jax.jit
+def return_statobjs(vertices, centres):
+    return jax.vmap(lambda c, v :jnp.array([v[0],v[1],v[2],v[3],c]))(centres,vertices)
+@jax.jit
+def return_states(randints,objrad, objsizes, rotations, limits, centres):
     #rotation will act as a multiplier on the value of the vertices, they will increment in the y by the inverse of the x
-    centres = jax.vmap(create_statobj, in_axes=(0,0,None))(objsizes,rotations,key)
-    vertices = jax.vmap(create_vertices)(rotations,centres,objsizes)
-    envbatch = jax.vmap(create_env,in_axes=(0,None))(randints,objrad)
-    size
-    statobjs = #shape is (vertex1, vertex2, vertex3) if more vertices exist ALL objects must have same number of elements, but can be 0
-    return envbatch, statobjs
+    #centres should be envnum, objnum
+    vertices, dists = jax.vmap(create_vertices)(rotations,centres,objsizes)
+    envbatch = jnp.array(jax.vmap(create_env,in_axes=(0,None))(randints,objrad))
+    print(centres[0])
+    #need to map over centres as well
+
+    statobjs = jax.vmap(return_statobjs,in_axes=(0,0))(vertices, centres)#shape is (vertex1, vertex2, vertex3) if more vertices exist ALL objects must have same number of elements, but can be 0
+    
+    #statobjs = centres, vertices
+    return envbatch, statobjs, dists
 #this calls once, can't be jitted because arrays need static shape
 def create_envbatch(key, envnum, limits, objnum, objrad,staticnum):
+    maxstatsize = 70
     #create envnum partitions, these are arrays containing
     #find a way to do tehis for an indeterminate number of objects?
     initstateints = jax.random.uniform(key, shape=(envnum,objnum+2,2),minval=limits[0]+objrad,maxval=limits[1]-objrad)
     #generate random sizes, positions for static objects and a rotation for these shapes
-    rotations = jax.random.uniform(key,shape=(envnum,staticnum),minval=0,maxval=2*math.pi)
-    objsizes = jax.random.uniform(key, shape=(envnum,staticnum),minval=5,maxval=40)
-    envstates, statobjs = return_states(initstateints,objrad,objsizes,rotations)# [n*[objnum+2* [x,y] ] [*4[x,y],(centrepoint)[x,y]]
-    print("init agent state: ",envstates[0][1])
-    return jnp.reshape(envstates,(envnum,objnum+2,3))
+    centres = jax.random.uniform(key,shape=(envnum,staticnum,2),minval=limits[0]+maxstatsize,maxval=limits[1]+maxstatsize)
+    rotations = jax.random.uniform(key,shape=(envnum,staticnum),minval=0,maxval=360)
+    objsizes = jax.random.uniform(key, shape=(envnum,staticnum,2),minval=5,maxval=40)
+    
+    envstates, statobjs, dists = return_states(initstateints,objrad,objsizes,rotations,limits,centres)# [n*[objnum+2* [x,y] ] [*4[x,y],(centrepoint)[x,y]]
+    print("init agent state: ",envstates[0][1]) 
+    return jnp.reshape(envstates,(envnum,objnum+2,3)), statobjs,rotations, dists
 key = jax.random.key(1204)

SNRLEnvironment/renderer.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import jax.numpy as jnp
 import matplotlib.animation as animation
+import jax
 
 
 
@@ -22,9 +23,23 @@ def drawshape(window,state,colour):
     radius = state[2].astype(int)
     window = tc.draw_circle(x,y,radius,colour,window)
     return window
+def drawrect(window,state):
+    print(state.shape)
+    vert1 = state[0].astype(int)
+    vert2 = state[1].astype(int)
+    vert3 = state[2].astype(int)
+    vert4 = state[3].astype(int)
+    print(vert1)
+    yellow = jnp.array([120,10,0])
+    window = tc.draw_line(vert1[0],vert1[1],vert2[0],vert2[1],radius=1,t=window,color=yellow)
+    window = tc.draw_line(vert3[0],vert3[1],vert4[0],vert4[1],radius=1,t=window,color=yellow)
+    window = tc.draw_line(vert1[0],vert1[1],vert3[0],vert3[1],radius=1,t=window,color=yellow)
+    window = tc.draw_line(vert2[0],vert2[1],vert4[0],vert4[1],radius=1,t=window,color=yellow)
+
+    return window
 def createplot():
     fig, ax = plt.subplots()
-def drawframe(states, window,collision):
+def drawframe(states, window,collision,staticstates):
     i=0
     for state in states:
         if i == 0:
@@ -37,6 +52,10 @@ def drawframe(states, window,collision):
             colour = jnp.array([0.8,0.1,0.2])
         window = drawshape(window,state,colour)
         i+=1
+    for statstate in staticstates:
+        print("stat state is: ",statstate)
+        window  = drawrect(window,statstate)
+    
     return window
     #plt.tight_layout()
     

SNRLEnvironment/training.py
@@ -25,7 +25,7 @@ devices = jax.devices()
 mesh = jax.make_mesh((len(devices),),('databatch'))
 sharding = jax.sharding.NamedSharding(mesh, P('databatch'))
 
-envnum = 60000 #number of environments
+envnum = 6 #number of environments
 episodelength = 200
 objnum = 18 #num of objects in environment
 rad = 10
@@ -48,8 +48,8 @@ def runstep(currentstates,actions):
     #extract first env state for image
     return currentstates, collision
 @jax.jit
-def createimages(state, window, collision):
-    frame = drawframe(state, window, collision)
+def createimages(state, window, collision, statobjs):
+    frame = drawframe(state, window, collision, statobjs)
     return frame
 @jax.jit
 def drawframes(envstates,window):
@@ -70,9 +70,12 @@ collision = 0
 #fig1 = plt.figure()
 while j < episodenum:
     i = 0
-    seed = 6870 * (j + 1)
+    seed = 800 * (j + 1)
     key = jax.random.key(seed)
-    envstates = create_envbatch(key, envnum,limits,objnum,10)
+    envstates, statobjs, rotations, dists = create_envbatch(key, envnum,limits,objnum,10,3)
+    print("Shape at batching is: ", jnp.array(statobjs).shape,statobjs[0])
+    print("rotation is ", rotations[0])
+    print("dists: ", dists)
     splitstates = jnp.split(envstates, len(devices))
     devind = 0
     for batch in splitstates:
@@ -99,7 +102,8 @@ while j < episodenum:
         if draw and j == episodenum -1:
             #frames = drawframes(envstates,window)
             firstenvcol.append(collision[0])
-            env1frame = createimages(envstates[0],window,collision[0])
+            print("shape is ",jnp.array(statobjs).shape)# should be 3,2 but is 30
+            env1frame = createimages(envstates[0],window,collision[0],statobjs[0])
             env1frames.append(env1frame)
         if i % 50 == 0:
             loss, actions = train_step(agent.policy,envstates,optimizer,collision)